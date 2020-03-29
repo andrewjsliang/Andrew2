1994df30dd61726b2df3c67ed4ecf3ab2b58e1b7
andrewjsliang <andrewjsliang@gmail.com>
2020-03-29 09:29:34 -0700

    Initial commit for the existing code

---
+++ Assets/Scripts/score.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class score : MonoBehaviour {
+
+   public Transform player;
+    
+    // Update is called once per frame
+    void Update() {
+        Debug.Log(player.position.z);
+    }    
+ }  
